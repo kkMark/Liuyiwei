8c3d2a369daec1274973d7baa4d25e2fc633a88a
黄锡凯 <781049207@qq.com>
2018-08-30 17:51:30 +0800

    放出登录页面=-=

--- Lywsale/Lywsale/App/AppDelegate/AppDelegate.m
+++ Lywsale/Lywsale/App/AppDelegate/AppDelegate.m
@@ -23,7 +23,6 @@
     
     // 测试的
     SetUserDefault(UserName, @"咕咕咕咕咕");
-    SetUserDefault(UserID, @"1");
     
     [[NSBundle bundleWithPath:@"/Applications/InjectionIII.app/Contents/Resources/iOSInjection.bundle"] load];
 #endif
