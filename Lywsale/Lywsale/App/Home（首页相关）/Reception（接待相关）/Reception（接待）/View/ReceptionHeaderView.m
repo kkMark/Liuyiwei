4fca203b4231dcb139d175292766f742f9cee929
黄锡凯 <781049207@qq.com>
2018-09-22 01:01:12 +0800

    完成接待页面，准备开始弄接待备注页面

--- Lywsale/Lywsale/App/Home（首页相关）/Reception（接待相关）/Reception（接待）/View/ReceptionHeaderView.m
+++ Lywsale/Lywsale/App/Home（首页相关）/Reception（接待相关）/Reception（接待）/View/ReceptionHeaderView.m
@@ -28,6 +28,7 @@
     
     NSArray *titles = @[@"咨询药品：", @"接待日期：", @"接待备注："];
     NSArray *contents = @[@"博路定", @"2018-05-13  18:11", @"报价了博路定的价格208元，顾客现场未定"];
+    float y = 0;
     for (int i = 0; i < titles.count; i++) {
         
         UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, self.nameLabel.maxY + 15 + 25 * i, 0, 0)];
@@ -42,12 +43,46 @@
         contentLabel.text = contents[i];
         contentLabel.font = [UIFont systemFontOfSize:14];
         contentLabel.textColor = [UIColor colorWithHexString:@"0x666666"];
-        contentLabel.textAlignment = NSTextAlignmentRight;
         contentLabel.numberOfLines = 0;
         contentLabel.height = [contentLabel getTextHeight];
         [self addSubview:contentLabel];
         
-        self.viewHeight = contentLabel.maxY + 15;
+        if (contentLabel.height > titleLabel.height) {
+            contentLabel.textAlignment = NSTextAlignmentLeft;
+        }
+        else contentLabel.textAlignment = NSTextAlignmentRight;
+
+        y = contentLabel.maxY + 15;
+    }
+    
+    for (int i = 0; i < 2; i++) {
+        
+        UIColor *color = i == 0 ? kMainColor : [UIColor colorWithHexString:@"0xF5A520"];
+        float width = 150;
+        float x = (ScreenWidth - width * 2) / 3;
+        
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake(0, y, width, 35);
+        btn.x = i == 0 ? x : x * 2 + width;
+        btn.layer.cornerRadius = btn.height / 2;
+        btn.layer.masksToBounds = YES;
+        btn.layer.borderWidth = 1;
+        btn.layer.borderColor = color.CGColor;
+        [self addSubview:btn];
+        
+        UIImageView *iconImgView = [[UIImageView alloc] initWithFrame:CGRectMake(25, 0, 24, 24)];
+        iconImgView.centerY = btn.height / 2;
+        iconImgView.image = [UIImage imageNamed:i == 0 ? @"reception-call" : @"reception-remark"];
+        [btn addSubview:iconImgView];
+        
+        UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(iconImgView.maxX + 10, 0, 0, btn.height)];
+        titleLabel.width = width - titleLabel.x;
+        titleLabel.text = i == 0 ? @"打电话" : @"接待备注";
+        titleLabel.font = [UIFont systemFontOfSize:15];
+        titleLabel.textColor = color;
+        [btn addSubview:titleLabel];
+        
+        self.viewHeight = btn.maxY + 15;
     }
 }
 
