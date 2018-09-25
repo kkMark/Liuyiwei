c8f6a706f9749fff8a021c8f038bf30a14809592
ALi <406252026@qq.com>
2018-09-25 15:07:53 +0800

    整了一丢丢

---
+++ Lywsale/Lywsale/Base（父类）/BaseTabelListView.m
@@ -0,0 +1,125 @@
+//
+//  BaseTabelListView.m
+//  Lywsale
+//
+//  Created by 尚洋 on 2018/9/24.
+//  Copyright © 2018年 黄锡凯. All rights reserved.
+//
+
+#import "BaseTabelListView.h"
+
+@implementation BaseTabelListView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        self.dataSource = [NSMutableArray array];
+        
+        [self initUI];
+    }
+    
+    return self;
+    
+}
+
+-(void)addTitleList:(NSArray *)array{
+    
+    _dataSource = [NSMutableArray arrayWithArray:array];
+    
+    [self.myTable reloadData];
+    
+}
+
+- (void)layoutSubviews{
+    
+    [super layoutSubviews];
+    
+    self.myTable.frame = self.bounds;
+    
+}
+
+-(void)initUI{
+    
+    self.myTable = [[UITableView alloc]initWithFrame:self.bounds style:UITableViewStyleGrouped];/**< 初始化_tableView*/
+    self.myTable.delegate = self;
+    self.myTable.dataSource = self;
+    self.myTable.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    self.myTable.backgroundColor = [UIColor clearColor];
+    [self addSubview:self.myTable];
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return self.dataSource.count;
+    
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+    
+}
+
+- (CGFloat)cellContentViewWith{
+    CGFloat width = [UIScreen mainScreen].bounds.size.width;
+    // 适配ios7
+    if ([UIApplication sharedApplication].statusBarOrientation != UIInterfaceOrientationPortrait && [[UIDevice currentDevice].systemVersion floatValue] < 8) {
+        width = [UIScreen mainScreen].bounds.size.height;
+    }
+    return width;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [self cellHeightForIndexPath:indexPath cellContentViewWidth:[self cellContentViewWith] tableView:self.myTable];
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    return nil;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 15;
+}
+
+/**< 每个分组下边预留的空白高度*/
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 15;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *identifier = @"BaseListTableViewCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:identifier];
+    }
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString *pushString = self.dataSource[indexPath.row];
+    
+    if (self.pushBlock) {
+        self.pushBlock(pushString);
+    }
+    
+}
+
+@end
