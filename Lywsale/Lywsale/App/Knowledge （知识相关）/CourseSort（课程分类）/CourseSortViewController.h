ee2c774e9f5b0542a9b8cd471653de4cc87f4cde
ALi <406252026@qq.com>
2018-09-30 09:33:47 +0800

    课程分类

---
+++ Lywsale/Lywsale/App/Knowledge （知识相关）/CourseSort（课程分类）/CourseSortViewController.h
@@ -0,0 +1,13 @@
+//
+//  CourseSortViewController.h
+//  Lywsale
+//
+//  Created by 尚洋 on 2018/9/29.
+//  Copyright © 2018年 黄锡凯. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CourseSortViewController : BaseViewController
+
+@end
---
+++ Lywsale/Lywsale/App/Knowledge （知识相关）/CourseSort（课程分类）/CourseSortViewController.m
@@ -0,0 +1,32 @@
+//
+//  CourseSortViewController.m
+//  Lywsale
+//
+//  Created by 尚洋 on 2018/9/29.
+//  Copyright © 2018年 黄锡凯. All rights reserved.
+//
+
+#import "CourseSortViewController.h"
+
+@interface CourseSortViewController ()
+
+@end
+
+@implementation CourseSortViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"课程分类";
+    
+    [self initUI];
+    
+}
+
+-(void)initUI{
+    
+    
+    
+}
+
+@end
