92275955dc7c686b3d765c304a24b097943ab829
ALi <406252026@qq.com>
2018-10-15 10:05:41 +0800

    二维码

--- Lywsale/Lywsale/App/Work（工作相关）/QrCode（二维码）/VC/QRMembersViewController.m
+++ Lywsale/Lywsale/App/Work（工作相关）/QrCode（二维码）/VC/QRMembersViewController.m
@@ -19,11 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    if (self.type == 0) {
-        self.view.backgroundColor = [UIColor blueColor];
-    }else{
-        self.view.backgroundColor = [UIColor yellowColor];
-    }
+//    if (self.type == 0) {
+//        self.view.backgroundColor = [UIColor blueColor];
+//    }else{
+//        self.view.backgroundColor = [UIColor yellowColor];
+//    }
     
     [self initUI];
     
@@ -52,7 +52,81 @@
     .heightIs(13);
     [qrTitleLabel setSingleLineAutoResizeWithMaxWidth:self.QRImageView.width];
     
+    UIView *leftView = [UIView new];
+    leftView.userInteractionEnabled = YES;
+    UITapGestureRecognizer *leftTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(left:)];
+    [leftView addGestureRecognizer:leftTap];
+    [self.view addSubview:leftView];
     
+    leftView.sd_layout
+    .leftEqualToView(qrTitleLabel)
+    .topSpaceToView(qrTitleLabel, 12)
+    .widthIs(50)
+    .heightIs(80);
+    
+    UIImageView *leftImageView = [UIImageView new];
+    leftImageView.image = [UIImage imageNamed:@"APP_WeChat"];
+    [leftView addSubview:leftImageView];
+    
+    leftImageView.sd_layout
+    .centerXEqualToView(leftView)
+    .topSpaceToView(leftView, 0)
+    .widthIs(50)
+    .heightEqualToWidth();
+    
+    UILabel *leftLabel = [UILabel new];
+    leftLabel.text = @"微信";
+    leftLabel.font = [UIFont systemFontOfSize:13];
+    [leftView addSubview:leftLabel];
+    
+    leftLabel.sd_layout
+    .centerXEqualToView(leftView)
+    .topSpaceToView(leftImageView, 10)
+    .heightIs(13);
+    [leftLabel setSingleLineAutoResizeWithMaxWidth:50];
+    
+    
+    UIView *rightView = [UIView new];
+    rightView.userInteractionEnabled = YES;
+    UITapGestureRecognizer *rightTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(right:)];
+    [rightView addGestureRecognizer:rightTap];
+    [self.view addSubview:rightView];
+    
+    rightView.sd_layout
+    .rightEqualToView(qrTitleLabel)
+    .topSpaceToView(qrTitleLabel, 12)
+    .widthIs(50)
+    .heightIs(80);
+    
+    UIImageView *rightImageView = [UIImageView new];
+    rightImageView.image = [UIImage imageNamed:@"APP_CircleOfFriends"];
+    [rightView addSubview:rightImageView];
+    
+    rightImageView.sd_layout
+    .centerXEqualToView(rightView)
+    .topSpaceToView(rightView, 0)
+    .widthIs(50)
+    .heightEqualToWidth();
+    
+    UILabel *rightLabel = [UILabel new];
+    rightLabel.text = @"朋友圈";
+    rightLabel.font = [UIFont systemFontOfSize:13];
+    [rightView addSubview:rightLabel];
+    
+    rightLabel.sd_layout
+    .centerXEqualToView(rightView)
+    .topSpaceToView(rightImageView, 10)
+    .heightIs(13);
+    [rightLabel setSingleLineAutoResizeWithMaxWidth:50];
+    
+    
+}
+
+-(void)left:(UITapGestureRecognizer *)sender{
+    
+}
+
+-(void)right:(UITapGestureRecognizer *)sender{
     
 }
 
